e5fd7d7d9c9ae3865fbd480803625fc3cca5d5df
Caio-nic <caio@nic.br>
2024-06-27 16:00:04 -0300

    Implement cryptography in password

--- www/app/Controller/UsersController.php
+++ www/app/Controller/UsersController.php
@@ -18,44 +18,6 @@ class UsersController extends AppController {
         }
 }
 
-// public function add() {
-//     if ($this->request->is('post')) {
-        
-//         // Obter a senha inserida pelo usuário
-//         $password = $this->request->data ['User']['password'];
-        
-//         // Criptografar a senha
-//         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        
-//         $this->User->create();
-//         $this->User->saveField('password', $hashed_password);
-        
-//         if ($this->User->save($this->request->data)) {
-//             $this->Flash->set('Your login has been saved.');
-//             $this->redirect(array('action' => 'login'));
-//         }
-//         $this->Flash->error(__('Não foi possível salvar o usuário. Por favor, tente novamente.'));
-//     }
-// }
-
 public function login() {}
+
 }
-
-// public function login() {
-//     if ($this->request->is('post')) {
-//             $email = $this->request->data['User']['email'];
-//     $password = $this->request->data['User']['password'];
-
-//     // Encontrar usuário pelo email
-//     $user = $this->User->findByEmail($email);
-
-//     if ($user && password_verify($password, $user['User']['password'])) {
-//         // Senha correta, iniciar sessão e redirecionar
-//         $this->Auth->login($user['User']);
-//         $this->redirect($this->Auth->redirectUrl());
-//     } else {
-//         // Senha incorreta, exibir mensagem de erro
-//         $this->Session->setFlash('Email ou senha incorretos.');
-//     }
-// }
-// }
--- www/app/Model/User.php
+++ www/app/Model/User.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('AppModel', 'Model');
+
 class User extends AppModel{
     // public $validate = array(
     //     'name' => array(
@@ -20,5 +21,42 @@ class User extends AppModel{
     //         'message' => 'Password invalid'
     //     ),
     // );
+
+
+    public $validate = array(
+        'name' => array(
+            'minLength' => array(
+                'rule' => array('minLength', '2'),
+            ),
+                'notBlank' => array(
+                    'rule' => array('notBlank'),
+                    'message' => 'Your Name is obrigatory'
+                )
+        ),
+        'email' => array(
+            'required' => array(
+                'rule' => 'notBlank',
+                'message' => 'Email é obrigatório'
+            ),
+            'valid' => array(
+                'rule' => 'email',
+                'message' => 'Por favor, insira um email válido'
+            )
+        ),
+        'password' => array(
+            'required' => array(
+                'rule' => 'notBlank', 
+                //posso colocar o regex aq tbm
+                'message' => 'Senha é obrigatória'
+            )
+        )
+    );
+    //criptografa a senha
+    public function beforeSave($options = array()) {
+        if (isset($this->data[$this->alias]['password'])) {
+            $this->data[$this->alias]['password'] = password_hash($this->data[$this->alias]['password'], PASSWORD_DEFAULT);
+        }
+        return true;
+    }
 }
 ?>
